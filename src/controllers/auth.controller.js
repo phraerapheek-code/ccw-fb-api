3b186a5a654ec3b417fc2cd06b8b91f8b19a5364
phraerapheek-code <phraeraphee.k@gmail.com>
2026-05-30 15:45:18 +0700

    add identity key

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,19 @@
-export function register(req, res) {
+import createHttpError from 'http-errors'
+import identityKeyUtil from '../utils/identity-key.util.js'
+
+export async function register(req, res, next) {
+     const {identity, firstName, lastName, password, confirmPassword} = req.body
+    // validation
+    if(!identity.trim() || !firstName.trim() || !lastName.trim() || !password.trim() || !confirmPassword.trim()) {
+        return next(createHttpError[400]('fill all inputs')) }
+    if(confirmPassword !== password) {
+        return next(createHttpError[400]('check confirm-password ')) }
+
+    const identityKey = identityKeyUtil(identity)
+
     res.json({
-        message : 'Register controller'
+        message : 'Register controller',
+        identityKey : identityKey
     })
 }
 
